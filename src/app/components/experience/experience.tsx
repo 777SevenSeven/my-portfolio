2681dae80d2a04016cfa093614088c15cd35304f
Andre <=>
2024-07-25 13:27:40 -0300

    commit feat: add skills component and experience bars

--- src/app/components/experience/experience.tsx
+++ src/app/components/experience/experience.tsx
@@ -1,15 +1,19 @@
-import { SectionTitle } from "../sectionTitle/sectionTitle"
+import { SectionTitle } from "../sectionTitle/sectionTitle";
 
-import "./experience.scss"
+import "./experience.scss";
+import { Skill } from "../skill/skill";
 
 export function Experience() {
-    return (
+    return(
         <div className="experience">
-        <SectionTitle text="Experience" />
-        <p>Texto</p>
-        <div className="experience-time">
-
+          <SectionTitle text="Experience"/>
+          <p>I'm currently studying Computer Engineering at UTFPR in the Third Period of the course. and 2 Year Of Study In Programmation, I won Competitions in the technology area.</p>
+          <div className="experience-time">
+            <Skill image="/react.png" measure={2} years="2 years"/>
+            <Skill image="/ts.png" measure={3} years="3 years"/>
+            <Skill image="/js.png" measure={3} years="3 years"/>
+            <Skill image="/java.png" measure={1} years="1 year"/>
+          </div>
         </div>
-      </div>
     )
 }
